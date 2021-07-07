413029fcd63bc618e685e39ddaafa8918b7ac827
Erdem Değer <erdemdeger11@hotmail.com>
2021-07-07 11:33:01 +0300

    Add batch transaction handler invokeBatch to TransactionClient - Pass TransactionClient.invokeBatch to sdk's withTransaction

--- lib/src/clients/transaction_client/transaction_client.dart
+++ lib/src/clients/transaction_client/transaction_client.dart
@@ -44,15 +44,10 @@ class TransactionClient extends pb.RpcClient {
     _wallet.networkInfo.tendermint34Client.close();
   }
 
-  /// A new transaction will be builded and signed from the given [request].
-  /// Broadcasts the signed transaction to the network and monitors its
-  ///   inclusion in a block.
-  ///
-  /// If the transaction is not included in a block before the provided timeout,
-  ///   throws an error.
-  ///
-  /// If the transaction is included in a block, checks for execution success.
-  ///   If not successful, throws an error.
+  /// [invoke] will be called from Protobuf generated API's to send
+  ///   transactions.
+  /// To send transactions by hand, or to send batched transactions call
+  ///   [invokeBatch] instead.
   @override
   Future<T> invoke<T extends pb.GeneratedMessage>(
     pb.ClientContext? ctx,
@@ -61,56 +56,84 @@ class TransactionClient extends pb.RpcClient {
     pb.GeneratedMessage request,
     T emptyResponse,
   ) async {
-    Future<bool> pollForTx({required String transactionHash}) async {
-      // Query the transaction using its hash.
-      final response = await _wallet.networkInfo.tendermint34Client.txSearch(
-        query: "tx.hash='$transactionHash'",
-        prove: true,
-      );
+    // For simplicity, we handle all the logic inside [invokeBatch],
+    //  this function will only call it, and will add [methodName] at the
+    //  start of the error, if there is any.
+    await invokeBatch([request])
+        .catchError((error) => throw ("$methodName $error"));
 
-      // If a response with given hash is found, check its code and events.
-      //  If code is different than 0, or events is empty, throw an error.
-      if (response.totalCount > 0) {
-        final txResult = response.txs[0].txResult;
-        if (txResult.code != 0 || txResult.events.isEmpty) {
-          throw ('$methodName call failed with code ${txResult.code} (log: ${txResult.log}, codespace: ${txResult.codeSpace})');
-        }
-      }
+    return emptyResponse;
+  }
 
-      return response.totalCount == 0;
-    }
-
-    Future<void> waitUntilTransactionInclusion({
-      required BroadcastTxSyncResponse response,
-      int timeoutMs = 60000,
-      int pollIntervalMs = 2000,
-    }) async {
-      final transactionHash = utf8.decode(response.hash);
-
-      var elapsedMilliseconds = 0;
-      while (await pollForTx(transactionHash: transactionHash)) {
-        if (elapsedMilliseconds >= timeoutMs) {
-          throw ('Transaction with hash $transactionHash was submitted but was not yet found on the chain. You might want to check later.');
-        }
-
-        await Future.delayed(Duration(milliseconds: pollIntervalMs));
-        elapsedMilliseconds += pollIntervalMs;
-      }
-    }
-
+  /// A new transaction will be builded and signed from the given [requests].
+  ///   Multiple requests will be batched together into one transaction.
+  /// Broadcasts the signed transaction to the network and monitors its
+  ///   inclusion in a block.
+  ///
+  /// If the transaction is not included in a block before the provided timeout,
+  ///   throws an error.
+  ///
+  /// If the transaction is included in a block, checks for execution success.
+  ///   If not successful, throws an error.
+  Future<void> invokeBatch(
+    List<pb.GeneratedMessage> requests, {
+    String? memo,
+  }) async {
     // Build and sign the transaction.
     final signedTx = await TxSigner().createAndSign(
       wallet: _wallet,
-      msgs: [request],
+      msgs: requests,
       fee: fee,
+      memo: memo,
     );
 
     // Broadcast the signed transaction.
     final response = await _wallet.networkInfo.tendermint34Client
         .broadcastTxSync(tx: signedTx.writeToBuffer());
 
-    await waitUntilTransactionInclusion(response: response);
+    // Wait until the transaction is in the block.
+    await _waitUntilTransactionInclusion(response: response);
+  }
 
-    return emptyResponse;
+  /// Checks the transaction by calling [Tendermint34Client.txSearch].
+  Future<bool> _pollForTx({required String transactionHash}) async {
+    // Query the transaction using its hash.
+    final response = await _wallet.networkInfo.tendermint34Client.txSearch(
+      query: "tx.hash='$transactionHash'",
+      prove: true,
+    );
+
+    // If a response with given hash is found, check its code and events.
+    //  If code is different than 0, or events is empty, throw an error.
+    if (response.totalCount > 0) {
+      final txResult = response.txs[0].txResult;
+      if (txResult.code != 0 || txResult.events.isEmpty) {
+        throw ('call failed with code ${txResult.code} (log: ${txResult.log}, codespace: ${txResult.codeSpace})');
+      }
+    }
+
+    return response.totalCount == 0;
+  }
+
+  /// Wait until the transaction is in the block. Checks the transaction by
+  ///   calling [_pollForTx] every [pollIntervlaMs] milliseconds.
+  /// If [timeoutMs] milliseconds is passed and transaction is still not found,
+  ///   throws an error.
+  Future<void> _waitUntilTransactionInclusion({
+    required BroadcastTxSyncResponse response,
+    int timeoutMs = 60000,
+    int pollIntervalMs = 2000,
+  }) async {
+    final transactionHash = utf8.decode(response.hash);
+
+    var elapsedMilliseconds = 0;
+    while (await _pollForTx(transactionHash: transactionHash)) {
+      if (elapsedMilliseconds >= timeoutMs) {
+        throw ('Transaction with hash $transactionHash was submitted but was not yet found on the chain. You might want to check later.');
+      }
+
+      await Future.delayed(Duration(milliseconds: pollIntervalMs));
+      elapsedMilliseconds += pollIntervalMs;
+    }
   }
 }
